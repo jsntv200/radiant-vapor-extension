579d2ca2d92b95acf35dc1972cee12a4b0276698
jsntv200 <jsntv200@gmail.com>
2011-08-22 17:50:09 +1000

    convert to radiant edge (f1129d1) extension format for bundler gemspec support

---
+++ lib/radiant-vapor-extension.rb
@@ -0,0 +1,8 @@
+module RadiantVaporExtension
+  VERSION     = "2.1.2"
+  SUMMARY     = "Provides an interface to redirect URLs in RadiantCMS"
+  DESCRIPTION = "Provides an interface to redirect URLs in RadiantCMS"
+  URL         = "http://github.com/saturnflyer/radiant-vapor-extension"
+  AUTHORS     = ["Jim Gay"]
+  EMAIL       = ["jim@saturnflyer.com"]
+end
