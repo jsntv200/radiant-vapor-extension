8caab2f0bb3f80a1efacb76b8d387978a067f07c
jsntv200 <jsntv200@gmail.com>
2011-08-22 18:58:44 +1000

    small style change

--- app/helpers/admin/page_node_alterations_helper.rb
+++ app/helpers/admin/page_node_alterations_helper.rb
@@ -3,10 +3,10 @@ module Admin::PageNodeAlterationsHelper
     result = ''
     display_name = @current_node.class.display_name
     unless display_name == 'Page'
-      result = %{<span class="info">(#{ h(display_name) })</span>}
+      result = %{<span class="info"><small>(#{ h(display_name) })</small></span>}
     end
     if flow_meter = @current_node.flow_meter
-      result << %{ <span class="info">Redirects to #{link_to flow_meter.redirect_url_for_display, flow_meter.redirect_url_for_display}</span>}
+      result << %{ <span class="info"><small>Redirects to #{link_to flow_meter.redirect_url_for_display, flow_meter.redirect_url_for_display}</small></span>}
     end
   end
 end
